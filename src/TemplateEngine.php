bb45530901062861b543619f5c5dd32f39ce5cf8
Jacques-Antoine <jacques.antoine.dev@gmail.com>
2024-10-28 21:00:15 +0100

    interpreteur {{ }}

--- src/TemplateEngine.php
+++ src/TemplateEngine.php
@@ -1,12 +1,26 @@
 <?php
 
 namespace JamCommits\TemplateEngine;
-class TemplateEngine {
-    public function render(string $templatePath, array $data = []): string {
-        extract($data);
 
-        ob_start();
-        include $templatePath;
-        return ob_get_clean();
+use Exception;
+
+class TemplateEngine {
+    /**
+     * @throws Exception
+     */
+    public function render(string $templatePath, array $data = []): string {
+        $templateContent = file_get_contents($templatePath);
+
+        if ($templateContent === false) {
+            throw new Exception("Impossible de charger le template : $templatePath");
+        }
+
+        return preg_replace_callback('/{{\s*(\w+)\s*}}/', function ($matches) use ($data) {
+            $variableName = $matches[1];
+            $value = $data[$variableName] ?? '';
+
+            // Prevent XSS
+            return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+        }, $templateContent);
     }
 }
